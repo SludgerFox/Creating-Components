b3a4cea41a1bdff56302677bf73e71e0d4f67a1e
SludgerFox <73816137+SludgerFox@users.noreply.github.com>
2022-09-27 14:33:48 -0500

    Added Create Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ParagraphComponent } from './paragraph/paragraph.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    HomeComponent,
+    ParagraphComponent
   ],
   imports: [
     BrowserModule
